b8fad6bec3fae7a21a8f825b2889e82ccc41d8dd
Efrain Rojas <erojas@zenlabs.net>
2018-03-05 14:10:26 -0400

    first commit

---
+++ app/src/main/java/com/example/zenlabs/busticketsonline/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.zenlabs.busticketsonline;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+public class MainActivity extends AppCompatActivity {
+    private WebView myWebView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+        myWebView = (WebView)findViewById(R.id.webView);
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        myWebView.loadUrl("https://busticketbolivia.herokuapp.com/");
+        myWebView.setWebViewClient(new WebViewClient());
+// Hide the status bar.
+
+    }
+}
